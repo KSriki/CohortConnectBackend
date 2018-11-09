f67a85fd1d5f1b973b65b5e3d8321e116d843edf
drubaby <peryton6@gmail.com>
2018-11-09 09:50:28 -0500

    added route and action controller for daily status

---
+++ app/controllers/daily_status_controller.rb
@@ -0,0 +1,27 @@
+class DailyStatusController < ApplicationController
+
+
+  def create
+    dailyStatus = DailyStatus.create(dailyStatus_params)
+    render json: dailyStatus.to_json
+
+  end
+
+  def show
+    dailyStatus = DailyStatus.find(params[:id])
+    render json: dailyStatus.to_json
+  end
+
+  def update
+    dailyStatus = DailyStatus.find(params[:id])
+    dailyStatus.update(status_params)
+    render json: dailyStatus.to_json
+  end
+
+  private
+  def status_params
+    params.require(:dailyStatus).permit(:status)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'daily_status/create'
+  get 'daily_status/show'
+  get 'daily_status/edit'
+  resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
