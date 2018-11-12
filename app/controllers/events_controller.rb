947a796abd180a113c6b65b688c60c0cf8ffb6ec
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2018-11-11 19:36:40 -0500

    added events routes and controller functions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+
+    def index
+        render json: Event.all
+    end
+
+    def show
+        event = Event.where(user_id: params[:id])
+        render json: event.to_json
+    end
+
+
 end
